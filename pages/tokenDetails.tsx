36ea89ff8c071de96f578afdde343f91c96f5bf2
Szymon Gos <szymonngos@gmail.com>
2022-04-21 12:11:17 +0100

    add primary button

--- pages/tokenDetails.tsx
+++ pages/tokenDetails.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import PrimaryButton from '@/components/PrimaryButton'
 
 export default function tokenDetails() {
   return (
     <section className='text-center'>
       <h1 className=' font-space-grotesk font-bold text-xl text-white'>Token details</h1>
+      <PrimaryButton title='primary button' />
     </section>
   )
 }
