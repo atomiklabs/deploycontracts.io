d1804b31154aacbc13670925b7555b3b4664a1ba
Szymon Gos <szymonngos@gmail.com>
2022-04-21 18:19:06 +0100

    add secondary button

--- components/buttons/SecondaryButton.tsx
+++ components/buttons/SecondaryButton.tsx
@@ -1,15 +1,10 @@
-import React, { useState } from 'react'
+const secondaryGradient = 'linear-gradient(122.48deg,#671BC9_-52.99%,#FD0F9E_97.26%)'
 
 export default function SecondaryButton({ title }: { title: string }) {
-  const [clicked, setClicked] = useState(false)
   return (
     <button
-      className='py-4 px-12 font-space-grotesk font-bold rounded-2xl text-[#FD0F9E] bg-transparent border-2 border-[linear-gradient(122.48deg,#671BC9_-52.99%,#FD0F9E_97.26%)] hover:border-none hover:text-white hover:bg-[linear-gradient(122.48deg,#671BC9_-52.99%,#FD0F9E_97.26%)] hover:shadow-[0px_2px_40px_rgba(253,15,158,0.7)]'
-      onClick={() => setClicked(true)}
-      style={{
-        background: clicked ? 'linear-gradient(122.48deg, #671BC9 -52.99%, #FD0F9E 97.26%)' : '',
-        color: clicked ? '#FFFFFF' : '',
-      }}
+      className={`px-12 py-4 font-space-grotesk font-bold text-[#FD0F9E] hover:text-white focus:text-white rounded-2xl hover:bg-[${secondaryGradient}] hover:border-none focus:border-none hover:shadow-[0px_2px_40px_rgba(253,15,158,0.7)] focus:bg-[${secondaryGradient}] transition-all duration-100 ease-out relative
+      `}
     >
       {title}
     </button>
