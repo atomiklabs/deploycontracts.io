4cce3281096ae99d80a7603c6f81842df24d71ac
Szymon Gos <szymonngos@gmail.com>
2022-04-21 16:12:25 +0100

    remove whitespace

--- pages/tokenDetails.tsx
+++ pages/tokenDetails.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function tokenDetails() {
   return (
     <section className='text-center'>
-      <h1 className=' font-space-grotesk font-bold text-xl text-white'>Token details</h1>
+      <h1 className='font-space-grotesk font-bold text-xl text-white'>Token details</h1>
     </section>
   )
 }
