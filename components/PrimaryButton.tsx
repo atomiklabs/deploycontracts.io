8be35751f27e3c1e2cfa7014f0962338b573c1dd
Szymon Gos <szymonngos@gmail.com>
2022-04-22 10:38:24 +0100

    remove variables, add letter spacing

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -1,9 +1,7 @@
-const primaryGradient = 'linear-gradient(115.82deg,#671BC9_5.15%,#FD0F9E_108.88%)'
-const secondaryGradient = 'linear-gradient(122.48deg,#671BC9_-52.99%,#FD0F9E_97.26%)'
 export default function PrimaryButton({ title }: { title: string }) {
   return (
     <button
-      className={`py-4 px-12 font-space-grotesk font-bold text-white rounded-2xl bg-[${primaryGradient}] hover:bg-[${secondaryGradient}] hover:shadow-[0px_2px_40px_rgba(253,15,158,0.7)] focus:bg-[${secondaryGradient}] transition-all duration-100 ease-out`}
+      className={`py-4 px-12 font-space-grotesk font-bold text-white rounded-2xl bg-[linear-gradient(115.82deg,#671BC9_5.15%,#FD0F9E_108.88%)] hover:bg-[linear-gradient(122.48deg,#671BC9_-52.99%,#FD0F9E_97.26%)] hover:shadow-[0px_2px_40px_rgba(253,15,158,0.7)] focus:bg-[linear-gradient(122.48deg,#671BC9_-52.99%,#FD0F9E_97.26%)] transition-all duration-100 ease-out tracking-widest`}
     >
       {title}
     </button>
