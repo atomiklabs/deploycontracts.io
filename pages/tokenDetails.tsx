897f54543da26148d2e2f0e24e54c7e4dd400ced
Szymon Gos <szymonngos@gmail.com>
2022-04-22 15:19:57 +0100

    add children prop

--- pages/tokenDetails.tsx
+++ pages/tokenDetails.tsx
@@ -4,7 +4,7 @@ export default function tokenDetails() {
   return (
     <section className='text-center'>
       <h1 className=' font-space-grotesk font-bold text-xl text-white'>Token details</h1>
-      <PrimaryButton title='primary button' />
+      <PrimaryButton>primary button</PrimaryButton>
     </section>
   )
 }
