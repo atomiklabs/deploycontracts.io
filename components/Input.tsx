8206ef98c6caebc56ccb965f00d0bca9df10566c
Szymon Gos <szymonngos@gmail.com>
2022-04-22 14:41:49 +0100

    init Input component

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,8 +1,12 @@
 export default function Input() {
   return (
-    <div className='flex flex-col'>
+    <div className='flex flex-col gap-y-2'>
       <label className='text-white'>Label</label>
-      <input type='text' placeholder='test' />
+      <input
+        className='py-4 px-5 bg-[#000B28] border-2 border-[#455378] outline-none focus:ring-0 rounded-2xl text-gray-100 placeholder:text-gray-300 focus:input-gradient '
+        type='text'
+        placeholder='test'
+      />
     </div>
   )
 }
