e24a4dba395449bfa62c360f11a1e8a2df773081
Szymon Gos <szymonngos@gmail.com>
2022-04-22 14:13:22 +0100

    include downloaded fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,12 +4,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <link
-            href='https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&family=Space+Grotesk:wght@700&display=swap'
-            rel='stylesheet'
-          />
-        </Head>
+        <Head />
         <body className='font-montserrat font-default text-base h-screen body-gradient'>
           <Main />
           <NextScript />
