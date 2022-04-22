d097c1f3bed6af1236bf2c2f4f819cd929c1d07b
Szymon Gos <szymonngos@gmail.com>
2022-04-22 20:28:51 +0100

    add responsiveness to tokenDetails

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,3 +1,3 @@
 export default function Container({ children }: { children: any }) {
-  return <div className='grid grid-cols-4 gap-x-6 sm:grid-cols-12 sm:gap-x-8'>{children}</div>
+  return <div className='grid grid-cols-4 gap-x-6 sm:grid-cols-12 sm:gap-x-8 px-6'>{children}</div>
 }
--- pages/tokenDetails.tsx
+++ pages/tokenDetails.tsx
@@ -7,17 +7,17 @@ export default function tokenDetails() {
   return (
     <section>
       <Container>
-        <div className='col-start-5 col-span-4 flex flex-col gap-y-[34px]'>
+        <div className='col-span-full sm:col-start-3 sm:col-span-8 xl:col-start-5 xl:col-span-4 flex flex-col gap-y-[34px]'>
           <h1 className='font-space-grotesk font-bold text-xl text-white'>Token details</h1>
           <p className='text-gray-100'>
             Token generations is.... consectetur adipiscing elit. Etiam pulvinar leo vitae massa congue euismod eget
             convallis tortor.
           </p>
         </div>
-        <div className='col-start-5 col-span-4'>
+        <div className='col-span-full sm:col-start-3 sm:col-span-8 xl:col-start-5 xl:col-span-4'>
           <SecondaryButton title='secondary button' />
         </div>
-        <form className='col-start-5 col-span-4 mt-12 flex flex-col gap-y-[52px]'>
+        <form className='col-span-full sm:col-start-3 sm:col-span-8 xl:col-start-5 xl:col-span-4 mt-12 flex flex-col gap-y-[52px]'>
           <Input label='Name of your token' placeholder='ABCD' />
           <Input label='Tottal supply' placeholder='1 000 000 000' />
           <PrimaryButton title='primary button' />
