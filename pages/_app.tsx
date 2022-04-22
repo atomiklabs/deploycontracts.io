1feafbb3c0996f2951b94be46f0727fc53053d80
Szymon Gos <szymonngos@gmail.com>
2022-04-22 10:17:17 +0100

    add utilities styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import 'tailwindcss/tailwind.css'
 import { AppProps } from 'next/app'
+import '../styles/index.scss'
 
 export default ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
   return <Component {...pageProps} />
