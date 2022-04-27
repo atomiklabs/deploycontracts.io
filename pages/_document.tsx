cd6ed7633773361a4cbfd96b514bc0d056b66318
Szymon Gos <szymonngos@gmail.com>
2022-04-27 09:01:59 +0100

    update fork main

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,14 +3,17 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html className='bg-[#031032]'>
         <Head>
           <link
             href='https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&family=Space+Grotesk:wght@700&display=swap'
             rel='stylesheet'
           />
         </Head>
-        <body className='font-montserrat font-default text-base'>
+        <body
+          className='font-montserrat font-default text-base h-screen'
+          style={{ background: 'linear-gradient(119.2deg, #071741 22.38%, #010C27 93.45%)' }}
+        >
           <Main />
           <NextScript />
         </body>
---
+++ pages/tokenDetails.tsx
@@ -0,0 +1,10 @@
+import PrimaryButton from '@/components/PrimaryButton'
+
+export default function tokenDetails() {
+  return (
+    <section className='text-center'>
+      <h1 className=' font-space-grotesk font-bold text-xl text-white'>Token details</h1>
+      <PrimaryButton>primary button</PrimaryButton>
+    </section>
+  )
+}
