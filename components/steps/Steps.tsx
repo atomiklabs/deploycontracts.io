554c3863201eb6eb45ca535a924a1a616d42d3ad
Szymon Gos <szymonngos@gmail.com>
2022-04-27 18:45:06 +0100

    add steps with logic

--- components/steps/Steps.tsx
+++ components/steps/Steps.tsx
@@ -1,17 +1,23 @@
-import Step1 from '@/components/steps/Step1'
-import Step2 from '@/components/steps/Step2'
-import Step3 from '@/components/steps/Step3'
-import Step4 from '@/components/steps/Step4'
+import { Step1Icon, Step2Icon, Step3Icon, Step4Icon, useStepIcon } from '@/components/steps/icons'
 
-export default function Steps() {
+type StepsProps = {
+  activeStep: number
+}
+
+export function Steps({ activeStep }: StepsProps) {
   return (
     <section>
       <div className='flex flex-row gap-x-4 justify-end'>
-        <Step1 height={48} width={48} fill='#671bc9' fillRule='nonzero' stroke='black' />
-        <Step2 height={48} width={48} />
-        <Step3 height={48} width={48} />
-        <Step4 height={48} width={48} />
+        {[Step1Icon, Step2Icon, Step3Icon, Step4Icon].map((StepIcon, idx) => (
+          <StepIcon key={idx} className='w-12 h-12' {...useStepIcon(idx, activeStep)} />
+        ))}
       </div>
     </section>
   )
+}
+
+export enum StepState {
+  ToBeVisited,
+  Current,
+  Visited,
 }
