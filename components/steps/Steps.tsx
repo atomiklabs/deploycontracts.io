6fe23d79f52e2cfc9ed21b1fdb1c038bd428a05f
Szymon Gos <szymonngos@gmail.com>
2022-04-28 11:20:52 +0100

    add tooltip component

--- components/steps/Steps.tsx
+++ components/steps/Steps.tsx
@@ -1,11 +1,15 @@
+import { useState } from 'react'
+import ReactTooltip from 'react-tooltip'
 import { Step1Icon, Step2Icon, Step3Icon, Step4Icon } from '@/components/steps/icons'
 import { useStepIcon } from '@/components/steps/useStepIcon'
 type StepsProps = {
   activeStep: number
 }
 
+const pageTitles = ['Token Details', 'Token Allocation', 'Marketing details', 'Summary']
+
 export function Steps({ activeStep }: StepsProps) {
-  console.log(activeStep)
+  const [hoveredId, sethoveredId] = useState<number | null>(null)
 
   return (
     <section>
@@ -13,13 +17,57 @@ export function Steps({ activeStep }: StepsProps) {
         {[Step1Icon, Step2Icon, Step3Icon, Step4Icon].map((StepIcon, idx) =>
           activeStep === idx ? (
             <div className='flex flex-1 items-center gap-x-4'>
-              <StepIcon key={idx} className='w-12 h-12 cursor-pointer' {...useStepIcon(idx, activeStep)} />
+              <StepIcon
+                key={idx}
+                className='w-12 h-12 cursor-pointer'
+                data-class='custom-tooltip'
+                data-tip={pageTitles.slice(idx, idx + 1)}
+                {...useStepIcon(idx, activeStep)}
+                onMouseOver={() => {
+                  sethoveredId(idx)
+                }}
+                onMouseLeave={() => {
+                  sethoveredId(null)
+                }}
+              />
               <span className='text-gray-100 font-medium whitespace-nowrap tracking-[.2rem]'>
                 STEP {activeStep + 1}/4
               </span>
+              <ReactTooltip
+                className='custom-tooltip'
+                textColor='#DCE2F2'
+                backgroundColor='#0F204C'
+                place='bottom'
+                type='dark'
+                effect='solid'
+              />
             </div>
           ) : (
-            <StepIcon key={idx} className='w-12 h-12 cursor-pointer self-end' {...useStepIcon(idx, activeStep)} />
+            <div>
+              <StepIcon
+                key={idx}
+                className='w-12 h-12 cursor-pointer self-end'
+                data-class='custom-tooltip'
+                data-tip={pageTitles.slice(idx, idx + 1)}
+                {...useStepIcon(idx, activeStep)}
+                onMouseOver={() => {
+                  sethoveredId(idx)
+                }}
+                onMouseLeave={() => {
+                  sethoveredId(null)
+                }}
+              />
+              {
+                <ReactTooltip
+                  className='custom-tooltip'
+                  textColor='#DCE2F2'
+                  backgroundColor='#0F204C'
+                  place='bottom'
+                  type='dark'
+                  effect='solid'
+                />
+              }
+            </div>
           ),
         )}
       </div>
