115429b0337cba90eba724ad5bd5fec7a1b9503a
Szymon Gos <szymonngos@gmail.com>
2022-04-28 10:21:41 +0100

    add flex to steps

--- components/steps/Steps.tsx
+++ components/steps/Steps.tsx
@@ -5,12 +5,23 @@ type StepsProps = {
 }
 
 export function Steps({ activeStep }: StepsProps) {
+  console.log(activeStep)
+
   return (
     <section>
-      <div className='flex flex-row gap-x-4 justify-end'>
-        {[Step1Icon, Step2Icon, Step3Icon, Step4Icon].map((StepIcon, idx) => (
-          <StepIcon key={idx} className='w-12 h-12' {...useStepIcon(idx, activeStep)} />
-        ))}
+      <div className='flex flex-row gap-x-4'>
+        {[Step1Icon, Step2Icon, Step3Icon, Step4Icon].map((StepIcon, idx) =>
+          activeStep === idx ? (
+            <div className='flex flex-1 items-center gap-x-4'>
+              <StepIcon key={idx} className='w-12 h-12 cursor-pointer' {...useStepIcon(idx, activeStep)} />
+              <span className='text-gray-100 font-medium whitespace-nowrap tracking-[.2rem]'>
+                STEP {activeStep + 1}/4
+              </span>
+            </div>
+          ) : (
+            <StepIcon key={idx} className='w-12 h-12 cursor-pointer self-end' {...useStepIcon(idx, activeStep)} />
+          ),
+        )}
       </div>
     </section>
   )
