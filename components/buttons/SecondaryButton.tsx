db0c3c0dc729e83cec71d8e13ce4efbcf3857456
Szymon Gos <szymonngos@gmail.com>
2022-04-28 19:31:59 +0100

    add Props type to SecondaryButton

--- components/buttons/SecondaryButton.tsx
+++ components/buttons/SecondaryButton.tsx
@@ -1,6 +1,14 @@
-export default function SecondaryButton({ children }: { children: any }) {
+type Props = {
+  children: any
+  onClick: () => void
+}
+
+export default function SecondaryButton({ children, ...props }: Props) {
   return (
-    <button className='secondary-button-gradient px-12 py-4 font-space-grotesk font-bold text-[#FD0F9E] hover:text-white active:text-white rounded-2xl hover:bg-[linear-gradient(122.48deg,#671BC9_-52.99%,#FD0F9E_97.26%)] hover:shadow-[0px_2px_40px_rgba(253,15,158,0.7)] active:bg-[linear-gradient(122.48deg,#671BC9_-52.99%,#FD0F9E_97.26%)] transition-all duration-100 ease-out tracking-widest'>
+    <button
+      className='secondary-button-gradient font-space-grotesk font-bold text-[#FD0F9E] hover:text-white active:text-white rounded-2xl hover:bg-[linear-gradient(122.48deg,#671BC9_-52.99%,#FD0F9E_97.26%)] hover:shadow-[0px_2px_40px_rgba(253,15,158,0.7)] active:bg-[linear-gradient(122.48deg,#671BC9_-52.99%,#FD0F9E_97.26%)] transition-all duration-100 ease-out tracking-widest'
+      {...props}
+    >
       {children}
     </button>
   )
