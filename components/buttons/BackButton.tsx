1a7ddca36a476ee7d163de1fa218781fcaec00e5
Szymon Gos <szymonngos@gmail.com>
2022-04-28 21:23:56 +0100

    add go back button

---
+++ components/buttons/BackButton.tsx
@@ -0,0 +1,7 @@
+export default function BackButton({ children }: { children: any }) {
+  return (
+    <button className='font-space-grotesk bg-clip-text text-transparent bg-[linear-gradient(115.82deg,#671BC9_5.15%,#FD0F9E_108.88%)] tracking-widest'>
+      {children}
+    </button>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
-import Hello from '@/components/Hello'
+import Link from 'next/link'
+import BackButton from '@/components/buttons/BackButton'
 
 export default function Example() {
   return (
     <div className='max-w-md mx-auto my-20'>
       <main>
-        <Hello />
+        <BackButton>
+          <Link href={'/'}>Back</Link>
+        </BackButton>
       </main>
     </div>
   )
