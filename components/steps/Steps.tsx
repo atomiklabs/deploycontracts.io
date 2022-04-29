27d0940c81f017f45e2a539eb3dbe85186dbf266
Szymon Gos <szymonngos@gmail.com>
2022-04-29 11:22:09 +0100

    rename key / remove repeatable code

--- components/steps/Steps.tsx
+++ components/steps/Steps.tsx
@@ -15,49 +15,34 @@ export function Steps({ activeStep }: StepsProps) {
   return (
     <section>
       <div className='flex flex-row gap-x-4'>
-        {[Step1Icon, Step2Icon, Step3Icon, Step4Icon].map((StepIcon, idx) =>
-          activeStep === idx ? (
-            <div key={`a-${idx}`} className='flex flex-1 items-center gap-x-4'>
-              <StepIcon
-                className='w-12 h-12 cursor-pointer'
-                data-for='custom-class'
-                data-tip={pageTitles.slice(idx, idx + 1)}
-                {...useStepIcon(idx, activeStep)}
-              />
+        {[Step1Icon, Step2Icon, Step3Icon, Step4Icon].map((StepIcon, idx) => (
+          <div
+            key={`step-icon-${idx}`}
+            className='flex items-center gap-x-4'
+            style={{ flex: activeStep === idx ? '1 0 0%' : 'none' }}
+          >
+            <StepIcon
+              className='w-12 h-12 cursor-pointer'
+              data-for='custom-class'
+              data-tip={pageTitles.slice(idx, idx + 1)}
+              {...useStepIcon(idx, activeStep)}
+            />
+            {activeStep === idx && (
               <span className='text-gray-100 font-medium whitespace-nowrap tracking-[.2rem]'>
                 STEP {activeStep + 1}/4
               </span>
-              <ReactTooltip
-                id='custom-class'
-                className='custom-tooltip'
-                textColor='#DCE2F2'
-                backgroundColor='#0F204C'
-                place='bottom'
-                type='dark'
-                effect='solid'
-              />
-            </div>
-          ) : (
-            <div key={`b-${idx}`}>
-              <StepIcon
-                className='w-12 h-12 cursor-pointer self-end'
-                data-for='custom-class'
-                data-tip={pageTitles.slice(idx, idx + 1)}
-                {...useStepIcon(idx, activeStep)}
-              />
-
-              <ReactTooltip
-                id='custom-class'
-                className='custom-tooltip'
-                textColor='#DCE2F2'
-                backgroundColor='#0F204C'
-                place='bottom'
-                type='dark'
-                effect='solid'
-              />
-            </div>
-          ),
-        )}
+            )}
+            <ReactTooltip
+              id='custom-class'
+              className='custom-tooltip'
+              textColor='#DCE2F2'
+              backgroundColor='#0F204C'
+              place='bottom'
+              type='dark'
+              effect='solid'
+            />
+          </div>
+        ))}
       </div>
     </section>
   )
