42500f30b63c1abe351a73b33854f8074a8df6fa
Szymon Gos <szymonngos@gmail.com>
2022-04-29 15:42:05 +0100

    update switch case

--- components/steps/useStepIcon.tsx
+++ components/steps/useStepIcon.tsx
@@ -1,8 +1,6 @@
 import { StepState } from '@/components/steps'
 
-type UseStepIconResult = { [k: string]: string }
-
-export function useStepIcon(currentStepIdx: number, activeStepIdx: number): UseStepIconResult {
+export function useStepIcon(currentStepIdx: number, activeStepIdx: number) {
   function getStepState(currentStepIdx: number, activeStepIdx: number): StepState {
     if (currentStepIdx < activeStepIdx) {
       return StepState.Visited
@@ -16,14 +14,22 @@ export function useStepIcon(currentStepIdx: number, activeStepIdx: number): UseS
   }
 
   switch (getStepState(currentStepIdx, activeStepIdx)) {
-    case StepState.ToBeVisited: {
-      return { fill: 'none', fillRule: 'nonzero', stroke: '#6075AA' }
-    }
-    case StepState.Current: {
-      return { fill: 'none', fillRule: 'nonzero', stroke: 'url(#current)' }
-    }
-    case StepState.Visited: {
-      return { fill: 'url(#visited)', fillRule: 'nonzero', stroke: 'black' }
-    }
+    case StepState.ToBeVisited:
+      if (currentStepIdx === 0) return { step: '/assets/step1-default.svg' }
+      if (currentStepIdx === 1) return { step: '/assets/step2-default.svg' }
+      if (currentStepIdx === 2) return { step: '/assets/step3-default.svg' }
+      if (currentStepIdx === 3) return { step: '/assets/step4-default.svg' }
+
+    case StepState.Current:
+      if (currentStepIdx === 0) return { step: '/assets/step1-current.svg' }
+      if (currentStepIdx === 1) return { step: '/assets/step2-current.svg' }
+      if (currentStepIdx === 2) return { step: '/assets/step3-current.svg' }
+      if (currentStepIdx === 3) return { step: '/assets/step4-current.svg' }
+
+    case StepState.Visited:
+      if (currentStepIdx === 0) return { step: '/assets/step1-visited.svg' }
+      if (currentStepIdx === 1) return { step: '/assets/step2-visited.svg' }
+      if (currentStepIdx === 2) return { step: '/assets/step3-visited.svg' }
+      if (currentStepIdx === 3) return { step: '/assets/step4-visited.svg' }
   }
 }
