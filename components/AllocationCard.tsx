864b5106171df8c120e032816a647bef2685cce8
Szymon Gos <szymonngos@gmail.com>
2022-04-30 01:27:16 +0100

    export colourPallete / add empty line - index.scss

--- components/AllocationCard.tsx
+++ components/AllocationCard.tsx
@@ -1,24 +1,7 @@
+import { useEffect, useState } from 'react'
 import Input from '@/components/Input'
 import Delete from '@/components/Delete'
-import { useEffect, useState } from 'react'
-
-const colourPallete = [
-  '#FD0F9E',
-  '#671BC9',
-  '#FD810F',
-  '#00D0FE',
-  '#FD3A0F',
-  '#BCFE00',
-  '#FDBA0F',
-  '#0CE2AF',
-  '#FE6B00',
-  '#BD01DC',
-  '#0084FE',
-  '#7EE42D',
-  '#4F14F9',
-  '#0DB427',
-  '#0EADAD',
-]
+import { colourPallete } from '@/utils/colourPallete'
 
 export default function AllocationCart({
   hideDeleteButton,
---
+++ utils/colourPallete.tsx
@@ -0,0 +1,17 @@
+export const colourPallete = [
+  '#FD0F9E',
+  '#671BC9',
+  '#FD810F',
+  '#00D0FE',
+  '#FD3A0F',
+  '#BCFE00',
+  '#FDBA0F',
+  '#0CE2AF',
+  '#FE6B00',
+  '#BD01DC',
+  '#0084FE',
+  '#7EE42D',
+  '#4F14F9',
+  '#0DB427',
+  '#0EADAD',
+]
