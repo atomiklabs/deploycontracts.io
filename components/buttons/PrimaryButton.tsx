e562022f5f18084c7c00ef4fae18dbed56a3a562
Szymon Gos <szymonngos@gmail.com>
2022-05-01 14:45:22 +0100

    next btn tooltip init

--- components/buttons/PrimaryButton.tsx
+++ components/buttons/PrimaryButton.tsx
@@ -1,9 +1,15 @@
-export default function PrimaryButton({ children }: { children: any }) {
+type Props = {
+  children: any
+  onClick?: () => void
+}
+
+export default function PrimaryButton({ children, ...props }: Props) {
   return (
     <button
       className={
-        'py-4 px-12 font-space-grotesk font-bold text-white rounded-2xl bg-[linear-gradient(115.82deg,#671BC9_5.15%,#FD0F9E_108.88%)] hover:bg-[linear-gradient(122.48deg,#671BC9_-52.99%,#FD0F9E_97.26%)] hover:shadow-[0px_2px_40px_rgba(253,15,158,0.7)] active:bg-[linear-gradient(122.48deg,#671BC9_-52.99%,#FD0F9E_97.26%)] transition-all duration-100 ease-out tracking-widest'
+        'font-space-grotesk font-bold text-white rounded-2xl bg-[linear-gradient(115.82deg,#671BC9_5.15%,#FD0F9E_108.88%)] hover:bg-[linear-gradient(122.48deg,#671BC9_-52.99%,#FD0F9E_97.26%)] hover:shadow-[0px_2px_40px_rgba(253,15,158,0.7)] active:bg-[linear-gradient(122.48deg,#671BC9_-52.99%,#FD0F9E_97.26%)] transition-all duration-100 ease-out tracking-widest w-full'
       }
+      {...props}
     >
       {children}
     </button>
--- pages/tokenAllocation.tsx
+++ pages/tokenAllocation.tsx
@@ -1,12 +1,22 @@
+import dynamic from 'next/dynamic'
 import { useState } from 'react'
 import Container from '@/components/Container'
 import AllocationCard from '@/components/AllocationCard'
 import SecondaryButton from '@/components/buttons/SecondaryButton'
 import ProgressBar from '@/components/ProgressBar'
+import PrimaryButton from '@/components/buttons/PrimaryButton'
+
+const freeSpace = 100
+
+const ReactTooltip = dynamic(() => import('react-tooltip'), {
+  ssr: false,
+})
 
 export default function tokenAllocation() {
   const [allocations, setAllocations] = useState([0])
   const [counter, setCounter] = useState(0)
+  const [disabledLink, setDisabledLink] = useState(false)
+  console.log(disabledLink)
 
   function addAllocation() {
     setCounter((prev) => (prev === 14 ? 0 : prev + 1))
@@ -19,8 +29,17 @@ export default function tokenAllocation() {
     setAllocations([...newAllocations])
   }
 
+  function checkFreeSpace() {
+    if (freeSpace > 100) {
+      setDisabledLink(true)
+    } else {
+      setDisabledLink(false)
+    }
+    console.log('clicked')
+  }
+
   return (
-    <section className='mt-10'>
+    <section className='mt-10 mb-20'>
       <Container>
         <div className='flex flex-col gap-y-[34px]'>
           <h1 className='font-space-grotesk font-bold text-xl text-white'>Token allocation</h1>
@@ -37,6 +56,21 @@ export default function tokenAllocation() {
           <SecondaryButton onClick={() => addAllocation()}>
             <div className='px-12 py-4'>Add new</div>
           </SecondaryButton>
+        </div>
+        <div className='relative mt-32 flex flex-col gap-y-6'>
+          <div className='relative z-20 py-4 px-6 text-base text-gray-100 flex flex-row gap-x-6 bg-[#341035] items-center justify-between rounded-3xl'>
+            <img src='/assets/error.svg' alt='error icon' className='w-[24px] h-[24px]' />
+            To go next step should use all off space.... Proin elementum nunc faucibus lacinia sollicitudin.
+            <div className='absolute bottom-[-5px] -z-10 right-[calc(50%-100px)] w-10 h-10 bg-[#341035] rotate-45'></div>
+          </div>
+          <div className='flex flex-row justify-between gap-x-16 md:gap-x-[144px]'>
+            <div></div>
+            <div className='flex-1'>
+              <PrimaryButton onClick={() => checkFreeSpace()}>
+                <div className='px-12 py-4 inline-block w-full h-full'>Next</div>
+              </PrimaryButton>
+            </div>
+          </div>
         </div>
       </Container>
     </section>
