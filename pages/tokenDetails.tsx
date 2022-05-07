e02851fd619eb1768e599e88fa54b0e44241bd51
Szymon Gos <szymonngos@gmail.com>
2022-05-07 21:35:14 +0100

    add useForm hook

--- pages/tokenDetails.tsx
+++ pages/tokenDetails.tsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
-import { useForm } from 'react-hook-form'
+import useForm from '@/utils/useForm'
 import PrimaryButton from '@/components/buttons/PrimaryButton'
 import SecondaryButton from '@/components/buttons/SecondaryButton'
 import Container from '@/components/Container'
 import Input from '@/components/Input'
 import { Steps } from '@/components/steps'
 
+const iniialValues = {
+  tokenName: '',
+  supply: '',
+}
+
 export default function tokenDetails() {
   const [activeStep, setActiveStep] = useState(0)
+  const { inputs, handleChange, handleSubmit } = useForm(iniialValues)
+
   return (
     <section className='mt-20'>
       <Container>
@@ -23,14 +30,30 @@ export default function tokenDetails() {
         <div className='mt-9 flex flex-col gap-y-[14px]'>
           <div className='text-white font-medium'>Minter adress</div>
 
-          <SecondaryButton>Connect your wallet</SecondaryButton>
+          <SecondaryButton>
+            <div className='px-12 py-4'>Connect your wallet</div>
+          </SecondaryButton>
         </div>
 
-        <form className='mt-12 flex flex-col gap-y-[52px]'>
-          <Input label='Name of your token' placeholder='Token name' />
-          <Input label='Tottal supply' placeholder='1 000 000 000' />
+        <form className='mt-12 flex flex-col gap-y-[52px]' onSubmit={handleSubmit}>
+          <Input
+            label='Name of your token'
+            placeholder='Token name'
+            name='tokenName'
+            value={inputs.tokenName}
+            handleChange={handleChange}
+          />
+          <Input
+            label='Tottal supply'
+            placeholder='1 000 000 000'
+            name='supply'
+            value={inputs.supply}
+            handleChange={handleChange}
+          />
 
-          <PrimaryButton>Next</PrimaryButton>
+          <PrimaryButton type='submit'>
+            <div className='px-12 py-4'>Next</div>
+          </PrimaryButton>
         </form>
       </Container>
     </section>
