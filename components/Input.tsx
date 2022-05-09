83cee98e81fd4a9aa867860af27fa32c68ea18e9
Szymon Gos <szymonngos@gmail.com>
2022-05-09 18:00:19 +0100

    remove textarea from input

--- components/Input.tsx
+++ components/Input.tsx
@@ -6,27 +6,18 @@ interface Props {
   textarea?: boolean
 }
 
-export default function Input({ label, placeholder, textarea }: Props) {
+export default function Input({ label, placeholder }: Props) {
   const [error, setError] = useState(false)
   return (
     <div className='flex flex-col gap-y-2'>
       <label className='text-white font-medium'>{label}</label>
-      {textarea ? (
-        <textarea
-          className='input min-h-[140px] resize-none py-4 px-5 bg-[#000B28] border-2 border-[#455378] rounded-2xl text-gray-100 placeholder:text-gray-300 visited:border-[#6075AA]'
-          style={{ borderColor: error ? `${defaultColors.error}` : 'none' }}
-          placeholder={placeholder}
-          required
-        ></textarea>
-      ) : (
-        <input
-          className='input py-4 px-5 bg-[#000B28] border-2 border-[#455378] rounded-2xl text-gray-100 placeholder:text-gray-300 visited:border-[#6075AA]'
-          style={{ borderColor: error ? `${defaultColors.error}` : 'none' }}
-          type='text'
-          placeholder={placeholder}
-          required
-        />
-      )}
+      <input
+        className='input py-4 px-5 bg-[#000B28] border-2 border-[#455378] rounded-2xl text-gray-100 placeholder:text-gray-300 visited:border-[#6075AA]'
+        style={{ borderColor: error ? `${defaultColors.error}` : 'none' }}
+        type='text'
+        placeholder={placeholder}
+        required
+      />
       {error && (
         <div className='flex flex-row gap-x-2 items-center'>
           <img src='/assets/error.svg' width={14} height={16} />
