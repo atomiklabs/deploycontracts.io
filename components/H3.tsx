219e701a01947d3d5f7a1d53d44ec03fc3562773
Szymon Gos <szymonngos@gmail.com>
2022-06-01 19:02:12 +0100

    add h3-heading

---
+++ components/H3.tsx
@@ -0,0 +1,7 @@
+export default function H3({ children }: { children: any }) {
+  return (
+    <h2 className='text-[36px] leading-[45.94px] md:text-[48px] md:leading-[61.25px] text-white font-space-grotesk font-bold'>
+      {children}
+    </h2>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import LinkButton from '@/components/buttons/LinkButton'
+import H3 from '@/components/H3'
 
 export default function Example() {
   return (
     <div className='max-w-md mx-auto my-20'>
       <main>
-        <LinkButton href='/'>Back</LinkButton>
+        <H3>SNIP-20 contract</H3>
       </main>
     </div>
   )
