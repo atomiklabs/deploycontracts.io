c1233313a95424bad41d21a9b793743a0bd468c6
Szymon Gos <szymonngos@gmail.com>
2022-06-02 14:26:36 +0100

    remove leading

--- components/ComingSoon.tsx
+++ components/ComingSoon.tsx
@@ -3,7 +3,7 @@ import hourglass from '../public/assets/hourglass.svg'
 
 export default function ComingSoon() {
   return (
-    <div className='flex flex-row items-center gap-x-4 text-sm leading-[20.42px] text-[#FDBA0F] font-space-grotesk font-medium uppercase tracking-[0.4rem] whitespace-nowrap'>
+    <div className='flex flex-row items-center gap-x-4 text-sm text-[#FDBA0F] font-space-grotesk font-medium uppercase tracking-[0.4rem] whitespace-nowrap'>
       <Image src={hourglass} alt='hourglass icon' height={24} width={24} layout='fixed' />
       Coming Soon
     </div>
