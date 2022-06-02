fb8b35f4c85b7bf2c91750c5157d69d2d86859af
Szymon Gos <szymonngos@gmail.com>
2022-06-02 22:04:08 +0100

    remove className

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,8 +1,6 @@
-export default function Container({ children, className }: { children: any; className?: string }) {
+export default function Container({ children }: { children: any }) {
   return (
-    <div
-      className={`max-w-[1200px] mx-auto grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12 lg:gap-x-8 gap-x-6 md:gap-x-8 ${className}`}
-    >
+    <div className='max-w-[1200px] mx-auto grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12 lg:gap-x-8 gap-x-6 md:gap-x-8'>
       {children}
     </div>
   )
