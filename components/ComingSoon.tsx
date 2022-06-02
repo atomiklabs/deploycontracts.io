248e144e6606ccdf99a0816d6ee670d10bad6384
Szymon Gos <szymonngos@gmail.com>
2022-06-02 22:10:54 +0100

    add nonOptimizeImage

--- components/ComingSoon.tsx
+++ components/ComingSoon.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image'
 import hourglass from '../public/assets/hourglass.svg'
+import { NonOptimizedImage } from './NonOptimizedImage'
 
 export default function ComingSoon() {
   return (
     <div className='flex flex-row items-center gap-x-4 text-sm text-[#FDBA0F] font-space-grotesk font-medium uppercase tracking-[0.4rem] whitespace-nowrap'>
-      <Image src={hourglass} alt='hourglass icon' height={24} width={24} layout='fixed' />
+      <NonOptimizedImage src={hourglass} alt='hourglass icon' height={24} width={24} layout='fixed' />
       Coming Soon
     </div>
   )
