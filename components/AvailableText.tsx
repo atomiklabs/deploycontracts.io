e2c3d6a77af04b1787b761b23ba3fede074eeb63
Szymon Gos <szymonngos@gmail.com>
2022-06-02 22:09:57 +0100

    add nonOptimizeImage

--- components/AvailableText.tsx
+++ components/AvailableText.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image'
+import { NonOptimizedImage } from './NonOptimizedImage'
 import check from '../public/assets/check.svg'
 
 export default function AvailableText() {
   return (
     <div className='flex flex-row items-center gap-x-4 text-sm text-[#0CE2AF] font-space-grotesk font-medium uppercase tracking-[0.4rem] whitespace-nowrap'>
-      <Image src={check} alt='check icon' height={24} width={24} layout='fixed' />
+      <NonOptimizedImage src={check} alt='check icon' height={24} width={24} layout='fixed' />
       Available
     </div>
   )
