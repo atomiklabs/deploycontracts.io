63c3c2ab59250ed89a25caff0cce5b6ce3273348
Szymon Gos <szymonngos@gmail.com>
2022-06-03 13:28:21 +0100

    add py to nav

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,9 +1,6 @@
-import { NonOptimizedImage } from './NonOptimizedImage'
 import GridContainer from '@/components/GridContainer'
 import SecondaryButton from '@/components/buttons/SecondaryButton'
 import { DeployconttractsLogo } from '@/components/DeployconttractsLogo'
-import menu from '../public/assets/menu.svg'
-import github from '../public/assets/github.svg'
 
 const navigation = [
   { name: 'About', href: '#' },
@@ -14,13 +11,15 @@ const navigation = [
 
 export default function Navigation() {
   return (
-    <nav className='p-5'>
+    <nav className='py-5'>
       <GridContainer>
         <div className='col-span-3 md:col-start-2 md:order-2 lg:col-start-1 flex items-center'>
-          <DeployconttractsLogo className='w-56 h-7 md:w-72 md:h-9 cursor-pointer' />
+          <DeployconttractsLogo className='w-56 h-full md:w-72 md:h-9 cursor-pointer shrink-0' />
         </div>
         <div className='col-span-1 md:col-start-1 md:order-1 lg:hidden flex justify-end md:justify-start'>
-          <NonOptimizedImage src={menu} alt='menu icon' className='w-8 h-5 cursor-pointer' />
+          <div className='w-8 h-full cursor-pointer flex items-center shrink-0'>
+            <img src='/assets/menu.svg' alt='menu icon' />
+          </div>
         </div>
         <ul className='md:col-start-8 md:order-3 lg:col-start-5 lg:col-span-8 lg:gap-x-12 flex flex-row text-gray-100 lg:order-2 justify-end items-center'>
           {navigation.map((x: any, i: number) => {
@@ -34,7 +33,7 @@ export default function Navigation() {
             <SecondaryButton>
               <a href='#' className='md:py-3 md:px-8 lg:px-12 flex flex-1 items-center gap-x-[10px]'>
                 <div className='w-6 h-7 flex justify-center'>
-                  <NonOptimizedImage src={github} alt='github logo' />
+                  <img src='/assets/github.svg' alt='github logo' />
                 </div>
                 Github
               </a>
