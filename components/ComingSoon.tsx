b086fc2c9a9c26033032b40ff2244da840844abe
Szymon Gos <szymonngos@gmail.com>
2022-06-03 12:21:27 +0100

    change to img tag

--- components/ComingSoon.tsx
+++ components/ComingSoon.tsx
@@ -1,10 +1,9 @@
-import hourglass from '../public/assets/hourglass.svg'
-import { NonOptimizedImage } from './NonOptimizedImage'
-
 export default function ComingSoon() {
   return (
     <div className='flex flex-row items-center gap-x-4 text-sm text-[#FDBA0F] font-space-grotesk font-medium uppercase tracking-[0.4rem] whitespace-nowrap'>
-      <NonOptimizedImage src={hourglass} alt='hourglass icon' height={24} width={24} layout='fixed' />
+      <div className='w-6 h-6'>
+        <img src={'/assets/hourglass.svg'} alt='hourglass icon' />
+      </div>
       Coming Soon
     </div>
   )
