37304755d4a464af15701f01b35c0061ec4c0ded
Szymon Gos <szymonngos@gmail.com>
2022-06-03 10:33:50 +0100

    move H1, H2 to a folder headings

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
-import H2 from '@/components/H2'
-import H1 from '@/components/H1'
+import H1 from '@/components/headings/H1'
+import H2 from '@/components/headings/H2'
 
 export default function LandingPage() {
   return (
     <>
       <main>
-        <H2>Avaible contracts</H2>
         <H1>Customize & Deploy</H1>
+        <H2>Avaible contracts</H2>
       </main>
     </>
   )
