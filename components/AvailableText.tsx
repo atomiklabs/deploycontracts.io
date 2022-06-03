db61bae16aa838d3901e39df487ac94fb3e76c22
Szymon Gos <szymonngos@gmail.com>
2022-06-03 11:55:41 +0100

    replace NonOptimizeImg with img tag

--- components/AvailableText.tsx
+++ components/AvailableText.tsx
@@ -1,10 +1,9 @@
-import { NonOptimizedImage } from './NonOptimizedImage'
-import check from '../public/assets/check.svg'
-
 export default function AvailableText() {
   return (
     <div className='flex flex-row items-center gap-x-4 text-sm text-[#0CE2AF] font-space-grotesk font-medium uppercase tracking-[0.4rem] whitespace-nowrap'>
-      <NonOptimizedImage src={check} alt='check icon' height={24} width={24} layout='fixed' />
+      <div className='w-6 h-6'>
+        <img src={'/assets/check.svg'} alt='check icon' />
+      </div>
       Available
     </div>
   )
