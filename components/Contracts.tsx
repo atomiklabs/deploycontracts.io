9de78b4597aed27a3be9acab6889efd0ec802a67
Szymon Gos <szymonngos@gmail.com>
2022-06-03 20:38:49 +0100

    add contracts

--- components/Contracts.tsx
+++ components/Contracts.tsx
@@ -1,9 +1,44 @@
 import Grid from '@/components/Grid'
 import H2 from '@/components/headings/H2'
+import H3 from '@/components/headings/H3'
+import AvailableText from '@/components/AvailableText'
+import ComingSoon from '@/components/ComingSoon'
+import PrimaryButton from '@/components/buttons/PrimaryButton'
 
 const data = [
   { id: 1, text: 'No smart contract developers required', icon: '/assets/hand.svg', iconName: 'hand' },
   { id: 2, text: "Fill up the form and submit, that's it", icon: '/assets/form.svg', iconName: 'form' },
+]
+
+const contracts = [
+  {
+    id: 1,
+    title: 'SNIP-20 contract',
+    text: 'SNIP-20 contract. Cras feugiat rhoncus augue, nec mattis ligula aliquet et. Morbi non porta lorem, aliquam suscipit leo. Maecenas blandit non sem vel blandit.',
+    available: true,
+    image: '/assets/disks.svg',
+  },
+  {
+    id: 2,
+    title: 'Vesting contract',
+    text: 'Lock your token for investors or individuals. Cras feugiat rhoncus augue, nec mattis ligula aliquet et. Morbi non porta lorem, aliquam suscipit leo. Maecenas blandit non sem vel blandit.',
+    available: false,
+    image: '',
+  },
+  {
+    id: 3,
+    title: 'IDO/liquidity pool contract',
+    text: 'Add real value for your token, and get ready for the market. Cras feugiat rhoncus augue, nec mattis ligula aliquet et. Morbi non porta lorem, aliquam suscipit leo. Maecenas blandit non sem vel blandit.',
+    available: false,
+    image: '',
+  },
+  {
+    id: 4,
+    title: 'Staking contract',
+    text: 'Lock your token for investors or individuals. Cras feugiat rhoncus augue, nec mattis ligula aliquet et. Morbi non porta lorem, aliquam suscipit leo. Maecenas blandit non sem vel blandit.',
+    available: false,
+    image: '',
+  },
 ]
 
 export default function Contracts() {
@@ -24,6 +59,56 @@ export default function Contracts() {
               </div>
             )
           })}
+        </div>
+
+        <div className='col-span-full mt-20'>
+          <Grid className='px-0 gap-y-6'>
+            {contracts.map((x) => {
+              return (
+                <>
+                  {x.available ? (
+                    <div className='col-span-full lg:col-span-full flex flex-col lg:flex-row p-6 md:p-12 lg:p-16 bg-[#0F204C] rounded-2xl'>
+                      <div className='flex justify-center lg:w-1/2 lg:order-2'>
+                        <img src={x.image} alt={`${x.title} image`} />
+                      </div>
+                      <div className='flex flex-col gap-y-5 mt-7 lg:w-1/2 lg:order-1'>
+                        <AvailableText />
+                        <H3>
+                          <h3>{x.title}</h3>
+                        </H3>
+                        <div className='text-gray-100 tracking-[-0.02rem]'>{x.text}</div>
+                        <PrimaryButton>
+                          <div className='py-3 px-11 whitespace-nowrap'>Create new token</div>
+                        </PrimaryButton>
+                      </div>
+                    </div>
+                  ) : (
+                    <div className='col-span-full lg:col-span-6 flex flex-col p-6 md:p-12 lg:p-16 gap-y-5 bg-[#0F204C] rounded-2xl'>
+                      <ComingSoon />
+                      <H3>
+                        <h3>{x.title}</h3>
+                      </H3>
+                      <div className='text-gray-100 tracking-[-0.02rem]'>{x.text}</div>
+                    </div>
+                  )}
+                </>
+              )
+            })}
+
+            <div className='col-span-full lg:col-span-6 flex flex-col gap-y-5 bg-[linear-gradient(115.82deg,#671BC9_5.15%,#FD0F9E_108.88%)] rounded-2xl p-6 md:p-12 lg:p-16'>
+              <H3>
+                <h3>Custom contract</h3>
+              </H3>
+              <div className='text-gray-100 tracking-[-0.02rem]'>
+                Do you need more complex contract? Please contact us and we would see how we can help.
+              </div>
+              <div className=''>
+                <PrimaryButton>
+                  <div className='py-3 px-11 whitespace-nowrap bg-white text-black'>Create new token</div>
+                </PrimaryButton>
+              </div>
+            </div>
+          </Grid>
         </div>
       </Grid>
     </section>
