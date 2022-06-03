e317b00cfd09a60ec5ee8b0689d4fec6dc9bfa2a
Szymon Gos <szymonngos@gmail.com>
2022-06-03 13:59:36 +0100

    minor fixes / replace NonOptimizeImg with img tag

--- components/Masthead.tsx
+++ components/Masthead.tsx
@@ -1,22 +1,25 @@
 import Grid from '@/components/Grid'
-import { NonOptimizedImage } from './NonOptimizedImage'
-import masthead from '../public/assets/masthead.svg'
+import H1 from '@/components/headings/H1'
 
 export default function Masthead() {
   return (
     <>
       <Grid>
-        <div className='col-span-full lg:col-start-8 lg:col-span-5 lg:order-2 flex justify-center'>
-          <NonOptimizedImage src={masthead} alt='masthead image' />
+        <div className='col-span-full lg:col-start-7 lg:col-span-6 lg:order-2 flex justify-center'>
+          <div className='h-full w-[270px] md:w-[410px] lg:w-[487px] flex shrink-0'>
+            <img src='/assets/masthead.svg' alt='masthead image' />
+          </div>
         </div>
-        <div className='col-span-full lg:col-start-1 lg:col-span-6 lg:order-1 text-white text-[64px] leading-[81px] text-center mt-9'>
-          <h1>
-            Customize & Deploy your smarts contract within minutes{' '}
-            <span className='relative z-10 whitespace-nowrap'>
-              <span className='bg-[linear-gradient(115.82deg,#671BC9_5.15%,#FD0F9E_108.88%)] w-full h-1/2 bottom-[-10%] absolute -z-10'></span>
-              for free
-            </span>
-          </h1>
+        <div className='col-span-full lg:col-start-1 lg:col-span-6 lg:order-1 mt-9 text-white text-center lg:text-left '>
+          <H1>
+            <h1>
+              Customize & Deploy your smarts contract within minutes{' '}
+              <span className='relative z-10 whitespace-nowrap'>
+                <span className='bg-[linear-gradient(115.82deg,#671BC9_5.15%,#FD0F9E_108.88%)] w-full h-[30%] bottom-[5%] absolute -z-10'></span>
+                for free
+              </span>
+            </h1>
+          </H1>
         </div>
       </Grid>
     </>
