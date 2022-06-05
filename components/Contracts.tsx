604257f219b5cfd6f6f206dcd96fe9f5bface37c
Szymon Gos <szymonngos@gmail.com>
2022-06-05 12:14:16 +0100

    add keys, remove ids

--- components/Contracts.tsx
+++ components/Contracts.tsx
@@ -4,40 +4,34 @@ import H3 from '@/components/headings/H3'
 import AvailableText from '@/components/AvailableText'
 import ComingSoon from '@/components/ComingSoon'
 import PrimaryButton from '@/components/buttons/PrimaryButton'
+import React from 'react'
 
 const data = [
-  { id: 1, text: 'No smart contract developers required', icon: '/assets/hand.svg', iconName: 'hand' },
-  { id: 2, text: "Fill up the form and submit, that's it", icon: '/assets/form.svg', iconName: 'form' },
+  { text: 'No smart contract developers required', icon: '/assets/hand.svg', iconName: 'hand' },
+  { text: "Fill up the form and submit, that's it", icon: '/assets/form.svg', iconName: 'form' },
 ]
 
 const contracts = [
   {
-    id: 1,
     title: 'SNIP-20 contract',
     text: 'SNIP-20 contract. Cras feugiat rhoncus augue, nec mattis ligula aliquet et. Morbi non porta lorem, aliquam suscipit leo. Maecenas blandit non sem vel blandit.',
     available: true,
     image: '/assets/disks.svg',
   },
   {
-    id: 2,
     title: 'Vesting contract',
     text: 'Lock your token for investors or individuals. Cras feugiat rhoncus augue, nec mattis ligula aliquet et. Morbi non porta lorem, aliquam suscipit leo. Maecenas blandit non sem vel blandit.',
     available: false,
-    image: '',
   },
   {
-    id: 3,
     title: 'IDO/liquidity pool contract',
     text: 'Add real value for your token, and get ready for the market. Cras feugiat rhoncus augue, nec mattis ligula aliquet et. Morbi non porta lorem, aliquam suscipit leo. Maecenas blandit non sem vel blandit.',
     available: false,
-    image: '',
   },
   {
-    id: 4,
     title: 'Staking contract',
     text: 'Lock your token for investors or individuals. Cras feugiat rhoncus augue, nec mattis ligula aliquet et. Morbi non porta lorem, aliquam suscipit leo. Maecenas blandit non sem vel blandit.',
     available: false,
-    image: '',
   },
 ]
 
@@ -51,9 +45,12 @@ export default function Contracts() {
           </H2>
         </div>
         <div className='col-span-full flex flex-col md:flex-row gap-y-8 md:gap-y-0 md:gap-x-20 mt-10 md:mt-14 lg:mt-16 justify-center items-center'>
-          {data.map((x) => {
+          {data.map((x, i) => {
             return (
-              <div className='flex flex-row max-w-xs md:text-lg text-white leading-[23px] gap-x-8 font-space-grotesk font-bold'>
+              <div
+                key={i}
+                className='flex flex-row max-w-xs md:text-lg text-white leading-[23px] gap-x-8 font-space-grotesk font-bold'
+              >
                 <img src={x.icon} alt={`${x.iconName} icon`} />
                 <span>{x.text}</span>
               </div>
@@ -63,9 +60,9 @@ export default function Contracts() {
 
         <div className='col-span-full mt-20'>
           <Grid className='px-0 gap-y-6'>
-            {contracts.map((x) => {
+            {contracts.map((x, i) => {
               return (
-                <>
+                <React.Fragment key={i}>
                   {x.available ? (
                     <div className='col-span-full lg:col-span-full flex flex-col lg:flex-row p-6 md:p-12 lg:p-16 bg-[#0F204C] rounded-2xl'>
                       <div className='flex justify-center lg:w-1/2 lg:order-2'>
@@ -91,7 +88,7 @@ export default function Contracts() {
                       <div className='text-gray-100 tracking-[-0.02rem]'>{x.text}</div>
                     </div>
                   )}
-                </>
+                </React.Fragment>
               )
             })}
 
