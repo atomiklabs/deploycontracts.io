7276f598f6e1594a5612975a8a2aeec57ac95765
Szymon Gos <szymonngos@gmail.com>
2022-06-05 19:56:09 +0100

    add Open Source bg-img

--- components/OpenSource.tsx
+++ components/OpenSource.tsx
@@ -4,14 +4,19 @@ import SecondaryButton from '@/components/buttons/SecondaryButton'
 
 export default function OpenSource() {
   return (
-    <section className=' mt-20'>
+    <section className='relative z-0 mt-20 lg:mt-60'>
       <GridContainer>
-        <div className='col-span-full flex justify-center'>
+        <div className='absolute hidden md:inline-block -z-10 md:top-[-5%] lg:top-[-20%] col-span-full'>
+          <div className='h-full w-full flex shrink-0'>
+            <img src='/assets/open-source-title.svg' alt='open source title image' />
+          </div>
+        </div>
+        <div className='col-span-full lg:col-start-7 lg:col-span-6 flex justify-center lg:order-2'>
           <div className='h-full w-[270px] md:w-[410px] lg:w-[487px] flex shrink-0'>
             <img src='/assets/open-source.svg' alt='open-source image' />
           </div>
         </div>
-        <div className='col-span-full flex flex-col gap-y-6 md:items-center'>
+        <div className='col-span-full lg:col-start-1 lg:col-span-6 flex flex-col gap-y-6 md:items-center lg:items-start lg:order-1'>
           <H2>
             <h2 className='text-center'>Open source</h2>
           </H2>
