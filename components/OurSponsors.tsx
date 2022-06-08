8c3d229a017b426cb7b4b41fa4107ea17590c5df
Szymon Gos <szymonngos@gmail.com>
2022-06-08 09:49:12 +0100

    add mobile screens

--- components/OurSponsors.tsx
+++ components/OurSponsors.tsx
@@ -1,4 +1,5 @@
 import GridContainer from '@/components/GridContainer'
+import LinkButton from './buttons/LinkButton'
 import H2 from './headings/H2'
 
 export default function OurSponsors() {
@@ -10,6 +11,16 @@ export default function OurSponsors() {
             <h2 className='text-center'>Our sponsors</h2>
           </H2>
           <div className='text-gray-100 tracking-[-0.002rem]'>This project is founded by multiple grant programs.</div>
+        </div>
+        <div className='col-span-full flex flex-col gap-y-6 mt-11 items-start'>
+          <div>
+            <img src='/assets/secret-network.svg' alt='secret network logo' />
+          </div>
+          <div className='text-lg text-white font-space-grotesk font-bold'>Secret Network Grant Program</div>
+          <div className='text-gray-100 tracking-[-0.002rem]'>
+            Founding for Applications, Ecosystem, Tooling and Network Imorovements.
+          </div>
+          <LinkButton href='#'>Visit webpage</LinkButton>
         </div>
       </GridContainer>
     </section>
