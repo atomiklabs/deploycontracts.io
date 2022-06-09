47c731a376c4a56b01b703bfb8500d27f8e45297
Szymon Gos <szymonngos@gmail.com>
2022-06-09 18:08:12 +0100

    add links to footer icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,5 @@
 import GridContainer from '@/components/GridContainer'
+import { landingPageUrl } from 'consts'
 import { AtomikLabsLogo } from './AtomikLabsLogo'
 
 export default function Footer() {
@@ -8,14 +9,14 @@ export default function Footer() {
         <div className='col-span-full flex flex-col items-center md:justify-between md:flex-row gap-y-6 py-14 md:py-6 md:border-t md:border-[#455378]'>
           <div className='flex flex-col md:flex-row gap-y-1 md:gap-x-2 items-center  text-gray-200'>
             Created by:
-            <span className='flex flex-row items-center gap-x-2'>
+            <a href={landingPageUrl.atomikLabs} target='_blank' className='flex flex-row items-center gap-x-2'>
               <AtomikLabsLogo className='fill-gray-200 w-6 h-8' />
               <span className='text-gray-200 font-bold'>Atomik Labs</span>
-            </span>
+            </a>
           </div>
-          <div className='cursor-pointer'>
+          <a href={landingPageUrl.github} target='_blank'>
             <img src='/assets/github.svg' alt='github icon' width={31} height={37} />
-          </div>
+          </a>
         </div>
       </GridContainer>
     </section>
