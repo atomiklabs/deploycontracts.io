fef910fbbfbe2b45a68df8e153cd66be324afbdf
Szymon Gos <szymonngos@gmail.com>
2022-06-10 18:01:23 +0100

    move header title to app.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,9 +4,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <title>deploycontracts.io</title>
-        </Head>
+        <Head />
         <body className='font-montserrat font-default text-base min-h-screen body-gradient'>
           <Main />
           <NextScript />
