59151ba5e9fbed1137b2f6a230057cda88906591
Tomasz Kopacki <tomasz@kopacki.net>
2022-06-18 18:54:50 +1000

    fix(env-vars): use public runtime config
    Enables Next.js to include selected env vars into the static export

--- next.config.js
+++ next.config.js
@@ -1,1 +1,14 @@
-module.exports = {}
+/** @type {import('next').NextConfig} */
+module.exports = {
+  publicRuntimeConfig: {
+    NETWORK: process.env.NETWORK,
+    CHAIN_ID: process.env.CHAIN_ID,
+    CHAIN_NAME: process.env.CHAIN_NAME,
+    CHAIN_GRPC: process.env.CHAIN_GRPC,
+    CHAIN_RPC: process.env.CHAIN_RPC,
+    CHAIN_REST: process.env.CHAIN_REST,
+    SNIPIX_SOURCODE_URL: process.env.SNIPIX_SOURCODE_URL,
+    CODE_ID: process.env.CODE_ID,
+    CODE_HASH: process.env.CODE_HASH,
+  },
+}
