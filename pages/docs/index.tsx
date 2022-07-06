e21f36ff661cd8b99f9869b7ea368f450e949229
Tomasz Kopacki <tomasz@kopacki.net>
2022-07-06 18:18:35 +1000

    fix(side-nav): remove highlighted style
    Makes all nav items look the same — no active state indicator

--- pages/docs/index.tsx
+++ pages/docs/index.tsx
@@ -423,7 +423,7 @@ export default function DocsPage({ chainSettings, metaStorageKey }: DocsPageProp
                   <ul className='mt-2 space-y-2 border-l-2 border-slate-700 lg:mt-4 lg:space-y-4'>
                     <li className='relative'>
                       <a
-                        className='cursor-pointer block w-full pl-3.5 before:pointer-events-none before:absolute before:-left-1 before:top-1/2 before:h-1.5 before:w-1.5 before:-translate-y-1/2 before:rounded-full text-[#FD0F9E] before:bg-progress-bar-0 before:block font-black'
+                        className='cursor-pointer block w-full pl-3.5 before:pointer-events-none before:absolute before:-left-1 before:top-1/2 before:h-1.5 before:w-1.5 before:-translate-y-1/2 before:rounded-full text-slate-500 before:hidden before:bg-slate-600 hover:text-slate-600 hover:before:block text-slate-400 before:bg-slate-700 hover:text-slate-300'
                         onClick={scrollToStarting}
                       >
                         Getting started
